517fa6c06bd594bf2cd36ec113e006477ae1ee06
ChaitanyaC27 <chaitanyapotbhare27@gmail.com>
2025-11-25 12:23:49 +0530

    final routing fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
   build: {
-    outDir: "dist"
+    outDir: 'dist',
   }
 })
